83aec746224b4342d2a993179072791ac88d3c65
Jitendra Mishra <jitendra.mishra@gmail.com>
2020-11-20 00:23:49 +0530

    waitdemo

--- PythonSelenium/explicitDemo.py
+++ PythonSelenium/explicitDemo.py
@@ -2,9 +2,11 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
 driver = webdriver.Firefox(executable_path="/usr/local/geckodriver")
-driver.implicitly_wait(5)
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 
 driver.find_element_by_css_selector("input.search-keyword").send_keys("ber")
@@ -18,9 +20,15 @@ for button in add_buttons:
     button.click()
 driver.find_element_by_css_selector("a.cart-icon").click()
 driver.find_element_by_xpath("//button[text()='PROCEED TO CHECKOUT']").click()
+
+wait = WebDriverWait(driver,8)
+
+wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "promoCode")))
+
 #time.sleep(2)
 driver.find_element_by_class_name("promoCode").send_keys("rahulshettyacademy")
 driver.find_element_by_css_selector(".promoBtn").click()
+wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "promoInfo")))
 #time.sleep(5)
 success_info = driver.find_element_by_class_name("promoInfo").text
 print(success_info)
